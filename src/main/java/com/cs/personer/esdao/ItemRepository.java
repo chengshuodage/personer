49b01aa1cceb89d41800b5284a3353939ca393f5
Jack <1783461699@qq.com>
2019-07-20 16:06:32 +0800

    es-api学习与测试

--- src/main/java/com/cs/personer/esdao/ItemRepository.java
+++ src/main/java/com/cs/personer/esdao/ItemRepository.java
@@ -4,7 +4,17 @@ import com.cs.personer.model.es.Item;
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+
 @Component
 public interface ItemRepository extends ElasticsearchRepository<Item, Long> {
+
+    /**
+     * @Description:根据价格区间查询
+     * @Param price1
+     * @Param price2
+     * @Author: https://blog.csdn.net/chen_2890
+     */
+    List<Item> findByPriceBetween(double price1, double price2);
 
 }
